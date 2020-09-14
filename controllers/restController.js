b15a8d9edd98397edbb11665d0d8776e13e0e624
Ann Huang <s52561@yahoo.com.tw>
2020-09-14 19:15:56 +0800

    feat:update this.categoryName

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,13 +1,13 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
-
-const restController = {
+let restController = {
   getRestaurants: (req, res) => {
     Restaurant.findAll({ include: Category }).then(restaurants => {
       const data = restaurants.map(r => ({
         ...r.dataValues,
-        description: r.dataValues.description.substring(0, 50)
+        description: r.dataValues.description.substring(0, 50),
+        categoryName: r.Category.name
       }))
       return res.render('restaurants', {
         restaurants: data
