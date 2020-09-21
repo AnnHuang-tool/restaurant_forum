caae26ff7137023f62bd096dfd3f3563efe4bb6f
Ann Huang <s52561@yahoo.com.tw>
2020-09-21 15:37:53 +0800

    refactor router

--- migrations/20200910084407-add-isAdmin-to-users.js
+++ migrations/20200910084407-add-isAdmin-to-users.js
@@ -1,25 +1,3 @@
-// 'use strict';
-
-// module.exports = {
-//   up: async (queryInterface, Sequelize) => {
-//     /**
-//      * Add altering commands here.
-//      *
-//      * Example:
-//      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-//      */
-//   },
-
-//   down: async (queryInterface, Sequelize) => {
-//     /**
-//      * Add reverting commands here.
-//      *
-//      * Example:
-//      * await queryInterface.dropTable('users');
-//      */
-//   }
-// };
-
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
