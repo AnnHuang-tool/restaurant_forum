c2381aeccdb79eeb472e2ef3fb9abaa4b2dc8239
Ann Huang <s52561@yahoo.com.tw>
2020-09-21 16:41:31 +0800

    feat:GET /api/admin/restaurants/:id

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -70,14 +70,8 @@ const adminController = {
   },
 
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, {
-      raw: true,
-      nest: true,
-      include: [Category]
-    }).then(restaurant => {
-      return res.render('admin/restaurant', {
-        restaurant: restaurant
-      })
+    adminService.getRestaurants(req, res, (data) => {
+      return res.render('admin/restaurant', data)
     })
   },
 
