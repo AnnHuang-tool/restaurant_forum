fcd9ac91d7962fe39a4d2eee0b1d2ca038aadb41
Ann Huang <s52561@yahoo.com.tw>
2020-09-23 16:10:26 +0800

    feat:POST /api/admin/categories - 後台新增類別

--- routes/apis.js
+++ routes/apis.js
@@ -12,6 +12,10 @@ router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
 router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
+// POST / api / admin / categories
+router.post('/admin/categories', categoryController.postCategory)
+// PUT / api / admin / categories /: id
+// DELETE / api / admin / categories /: id
 router.get('/admin/categories', categoryController.getCategories)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 module.exports = router
